9aa034925798d87c2eccd9b01ea65ff907e225cf
Arshdeep-Singh-01 <itsarshdeepsingh02@gmail.com>
2021-11-06 16:43:36 +0530

    Balanced paranthesis : started

--- tree.c
+++ tree.c
@@ -1,21 +1,35 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+
 struct Tree{
     int data;
     int number_of_children;
     struct Tree** children; 
 }typedef tree;
 
+
+struct Node{
+    int posn;
+    char pr;
+}typedef node;
+
+int count=0;
 int number_of_nodes=0;
+node* bp=NULL;
 
 void addNode(tree ** k);
 void display(tree** r);
+void balanced_paranthesis(tree ** k);
+void display_bp(void);
 
 int main(){
     tree * root=NULL;
     addNode(&root);
     display(&root);
+    bp=(node*)malloc(2*sizeof(node)*number_of_nodes);
+    balanced_paranthesis(&root);
+    display_bp();
     return 0;
 }
 
@@ -33,12 +47,36 @@ void addNode(tree ** k){
     for(int i=0;i<t;i++){
         printf("\nAdd child %d of node %d:\n",i+1,(*k)->data);
         addNode(&((*k)->children[i]));
-    }   
+    }  
 }
 
 void display(tree** r){
     printf("%d ",(*r)->data); 
     for(int i=0;i<(*r)->number_of_children;i++){
         display(&((*r)->children[i]));
-    }   
+    }
+    printf("\n");  
+}
+
+void display_bp(void){
+    for(int i=0;i<2*number_of_nodes;i++){
+        printf("%c ",bp[i].pr);
+    }
+    printf("\n");
+    for(int i=0;i<2*number_of_nodes;i++){
+        printf("%d ",bp[i].posn);
+    }
+    printf("\n");
+}
+
+void balanced_paranthesis(tree ** r){
+    bp[count].posn=(*r)->data;
+    bp[count].pr='(';
+    count++;
+    for(int i=0;i<(*r)->number_of_children;i++){
+        balanced_paranthesis(&((*r)->children[i]));
+    }
+    bp[count].posn=(*r)->data;
+    bp[count].pr=')';
+    count++;
 }
