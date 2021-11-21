43f715917a4550174c5f840810f20fc27dcf5449
inayat-kaur <2020csb1088@iitrpr.ac.in>
2021-11-21 23:50:49 +0530

    final project

--- depth_first_unary_degree_sequence.c
+++ depth_first_unary_degree_sequence.c
@@ -8,16 +8,16 @@ static int count=0;
 
 int number_of_nodes=0;
 
-node* dfuds=NULL;
+node* dfuds=NULL;       // pointer to the array of structures storing the parentheses and node data
 
 void DFUDS(tree ** k,int n){
     number_of_nodes=n;
-    dfuds=(node*)malloc(2*sizeof(node)*number_of_nodes);
+    dfuds=(node*)malloc(2*sizeof(node)*number_of_nodes);    //allocating memory for the parentheses sequence
     dfuds[count].val=(*k)->data;
     dfuds[count].pr='(';
     count++;
-    depth_first(k);
-    display_dfuds();
+    depth_first(k);             //performing depth first traversal and storing the nodes of the tree
+    display_dfuds();            //Displaying the stored tree
     int res;
     for( int i = 0;i < 2*number_of_nodes; i++){
         if(dfuds[i].pr == '('){
@@ -39,7 +39,7 @@ void DFUDS(tree ** k,int n){
     }
 }
 
-void depth_first(tree ** r){
+void depth_first(tree ** r){                //performing depth first search on the tree and storing values and parentheses for the nodes
     for(int i=0;i<(*r)->number_of_children;i++){
          dfuds[count].val=(*r)->children[i]->data;
          dfuds[count].pr='(';
@@ -56,16 +56,16 @@ void depth_first(tree ** r){
 void display_dfuds(void){ 
     printf("DEPTH FIRST UNARY DEGREE SEQUENCE (DFUDS) REPRESENTATION\n");
      for(int i=0;i<2*number_of_nodes;i++){
-        printf("%c ",dfuds[i].pr);
+        printf("%c ",dfuds[i].pr);      //displaying the parantheses sequence
     }
     printf("\n");
     for(int i=0;i<2*number_of_nodes;i++){
-        printf("%d ",dfuds[i].val);
+        printf("%d ",dfuds[i].val);         //displaying the values of the nodes in the parantheses sequence
     }
     printf("\n");
 }
 
-int find_close_dfuds(int i)
+int find_close_dfuds(int i)     //function to find closing bracket corresponding to the opening bracket at index i
 {
     int temp = dfuds[i].val;
     for(int j=i+1; j<2*number_of_nodes; j++)
@@ -77,7 +77,7 @@ int find_close_dfuds(int i)
     }
 }
 
-int find_open_dfuds(int i){
+int find_open_dfuds(int i){         //function to find opening bracket corresponding to the closing bracket at index i
     int temp = dfuds[i].val;
     for(int j=i-1; j>-1; j--)
     {
@@ -88,7 +88,7 @@ int find_open_dfuds(int i){
     }
 }
 
-int enclose_dfuds(int i){
+int enclose_dfuds(int i){           //function to find the opening bracket which encloses the opening bracket at index i
     int count = 0;
     for(int j = i - 1; j > -1 ; j--){
         if(dfuds[j].pr == '('){
@@ -103,13 +103,13 @@ int enclose_dfuds(int i){
     }
 }
 
-int rank_dfuds(char* pattern,int size,int i){
+int rank_dfuds(char* pattern,int size,int i){       //function to find the rank of a given pattern till index i using Z-array method
     int c =0;
     int p=0;
     int a, b, k;
     int l=size+1+i;
-    int * Z_array=(int *)malloc(l*sizeof(int));
-    char * new_string=(char*)malloc((l+1)*sizeof(char));
+    int * Z_array=(int *)malloc(l*sizeof(int));         //allocating memory for the Z-array
+    char * new_string=(char*)malloc((l+1)*sizeof(char));        //allocating memory for the concatinated string of pattern and parantheses
     while(p<size){
         new_string[p]=pattern[p];
         p++;
@@ -123,7 +123,7 @@ int rank_dfuds(char* pattern,int size,int i){
     new_string[p]='\0';
     a=0;
     b=0;
-    for (int j=1;j<l;j++){
+    for (int j=1;j<l;j++){                                      //finding rank using Z-algorithm
         if(j>b){
             a=j;
             b=j;
@@ -144,21 +144,21 @@ int rank_dfuds(char* pattern,int size,int i){
             }
         }
     }
-    for(p=1;p<l;p++){
-        if(Z_array[p]==size)c++;
+    for(p=1;p<l;p++){               
+        if(Z_array[p]==size)c++;        //calculating the number of occurrences of the pattern
     }
-    free(Z_array);
+    free(Z_array);          //freeing the allocated memory
     free(new_string);
     return c;
 }
 
-int select_dfuds(char* pattern,int size,int i){
+int select_dfuds(char* pattern,int size,int i){           //function to find the index of the ith occurrence of the patten using Z-array method
     int c=0;
     int p=0;
     int a, b, k;
     int l=size+1+(2*number_of_nodes);
-    int * Z_array=(int *)malloc(l*sizeof(int));
-    char * new_string=(char*)malloc((l+1)*sizeof(char));
+    int * Z_array=(int *)malloc(l*sizeof(int));          //allocating memory for the Z-array
+    char * new_string=(char*)malloc((l+1)*sizeof(char));        //allocating memory for the concatinated string of pattern and parantheses
     while(p<size){
         new_string[p]=pattern[p];
         p++;
@@ -173,7 +173,7 @@ int select_dfuds(char* pattern,int size,int i){
     a=0;
     b=0;
     for (int j=1;j<l;j++){
-        if(j>b){
+        if(j>b){                                                //Calculating select using Z-array algorithm
             a=j;
             b=j;
             while ((new_string[b-a]==new_string[b])&&(b<l))b++;
@@ -194,31 +194,31 @@ int select_dfuds(char* pattern,int size,int i){
         }
     }
     for(p=1;p<l;p++){
-        if(Z_array[p]==size)c++;
+        if(Z_array[p]==size)c++;                //finding the index of ith occurrence of the pattern
         if(c==i){
             free(Z_array);
             free(new_string);
             return p-size;
         } 
     }
-    free(Z_array);
+    free(Z_array);                   //freeing the allocated memory
     free(new_string);
-    return -1;
+    return -1;          //returning -1 if pattern didn't occur ith time
 }
 
-int parent_dfuds(int v){
+int parent_dfuds(int v){            //function to find the parent of the node at index i
     if(dfuds[v].val==1) return -1;
     int closed = rank_dfuds(")",1,v+1);
     return select_dfuds(")",1,1+closed);
 }
 
-int last_child_dfuds(int i){
+int last_child_dfuds(int i){            //function to find the last child of the node at index i
     int v=select_dfuds(")",1,dfuds[i].val); 
     int h=rank_dfuds("(",1,v);
     return select_dfuds("(",1,h);                           
 }
 
-int first_child_dfuds(int i){
+int first_child_dfuds(int i){           //function to find the first child of the node at index i
     if(i==0){
         if(dfuds[1].pr=='(') return 2;
         else return -1;
@@ -228,12 +228,12 @@ int first_child_dfuds(int i){
     else return -1;                                             
 }
 
-int degree_dfuds(int v){
+int degree_dfuds(int v){                //function to find the degree of the node at index i
     if(v==1) return select_dfuds(")",1,v)-2;
     return select_dfuds(")",1,v) - select_dfuds(")",1,v-1) - 1;                         
 }
 
-int leaf_rank_dfuds(int v){
+int leaf_rank_dfuds(int v){              //function to find the rank of the node at index i
     int g=dfuds[v].val;
     int h=select_dfuds(")",1,g);
     int r=rank_dfuds("))",2,h);
@@ -241,7 +241,7 @@ int leaf_rank_dfuds(int v){
     return r;
 }
 
-int leaf_select_dfuds(int i){
+int leaf_select_dfuds(int i){            //function to find the leaf select 
     return 1+select_dfuds("))",2,i);
 }
 
