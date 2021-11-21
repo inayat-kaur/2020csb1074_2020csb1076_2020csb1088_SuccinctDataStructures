e4e425d581a7f5ec4d8078919bdfd05b9141da51
inayat-kaur <2020csb1088@iitrpr.ac.in>
2021-11-21 21:43:21 +0530

    first child function

--- depth_first_unary_degree_sequence.c
+++ depth_first_unary_degree_sequence.c
@@ -18,30 +18,26 @@ void DFUDS(tree ** k,int n){
     count++;
     depth_first(k);
     display_dfuds();
-
     int res;
     for( int i = 0;i < 2*number_of_nodes; i++){
         if(dfuds[i].pr == '('){
             printf("FOR %d\n",dfuds[i].val);
             printf("Parent of %d : ",dfuds[i].val);
-            if((res = parent_dfuds(i))!=-1){ 
-                printf("%d\n",res);
+            res = parent_dfuds(i);
+            if(res!=-1){ 
+                printf("%d\n",dfuds[res-1].val);
             }
-            printf("Subtree size of %d : %d\n",dfuds[i].val,subtreesize_dfuds(i));
-            printf("degree of %d : %d\n",dfuds[i].val,degree_dfuds(i));
-            printf("Preorder rank of %d : %d\n",dfuds[i].val,preorder_rank_dfuds(i));
-            printf("Preorder select of %d : %d\n",dfuds[i].val,preorder_select_dfuds(i));
+            else if(res==-1) printf("ROOT ITSELF\n");
+            if(first_child_dfuds(i)!=-1) printf("First child of %d : %d\n",dfuds[i].val,dfuds[first_child_dfuds(i)-1].val);
+            else printf("First child of %d : NULL\n",dfuds[i].val);
+            if(first_child_dfuds(i)!=-1) printf("Last child of %d : %d\n",dfuds[i].val,dfuds[last_child_dfuds(i)-1].val);
+            else printf("Last child of %d : NULL\n",dfuds[i].val);
+            printf("degree of %d : %d\n",dfuds[i].val,degree_dfuds(dfuds[i].val));
             printf("Leaf-rank of %d : %d\n",dfuds[i].val,leaf_rank_dfuds(i));
             printf("Leaf-select of %d : %d\n",dfuds[i].val,leaf_select_dfuds(i));
-            printf("Inorder rank of %d : %d\n",dfuds[i].val,inorder_rank_dfuds(i));
-            printf("Inorder select of %d : %d\n",dfuds[i].val,inorder_select_dfuds(i));
-            printf("Leftmost leaf of %d : %d\n",dfuds[i].val,leftmost_leaf_dfuds(i));
-            printf("Rightmost leaf of %d : %d\n",dfuds[i].val,rightmost_leaf_dfuds(i));
+            printf("Leftmost leaf of %d : %d\n",dfuds[i].val,dfuds[leftmost_leaf_dfuds(i)-1].val);
         }
     }
-//int child_dfuds(int v, int i);
-//lac function left
-//functions to be checked
 }
 
 void depth_first(tree ** r){
@@ -211,30 +207,35 @@ int select_dfuds(char* pattern,int size,int i){
 }
 
 int parent_dfuds(int v){
-    return select_dfuds(")",1,rank_dfuds(")",1,find_open_dfuds(v-1))) + 1;
+    if(dfuds[v].val==1) return -1;
+    int closed = rank_dfuds(")",1,v+1);
+    return select_dfuds(")",1,1+closed);
 }
 
-int child_dfuds(int v, int i){
-    return find_close_dfuds(select_dfuds(")",1,rank_dfuds(")",1,v)+1)- i) + 1;
+int last_child_dfuds(int i){
+    int v=select_dfuds(")",1,dfuds[i].val); 
+    int h=rank_dfuds("(",1,v);
+    return select_dfuds("(",1,h);                           
 }
 
-int subtreesize_dfuds(int v){
-    return find_close_dfuds(enclose_dfuds(v)-v)/2 +1;
+int first_child_dfuds(int i){
+    if(i==0){
+        if(dfuds[1].pr=='(') return 2;
+        else return -1;
+    }
+    int v=select_dfuds(")",1,dfuds[i].val-1); 
+    if(dfuds[v].pr=='(') return v+1; 
+    else return -1;                                             
 }
 
 int degree_dfuds(int v){
-    return select_dfuds(")",1,v) - select_dfuds("(",1,v) - 1;
-}
-
-int preorder_rank_dfuds(int v){
-    return rank_dfuds(")",1,v-1)+1;
-}
-
-int preorder_select_dfuds(int i){
-    return select_dfuds(")",1,i-1)+1;
+    if(v==1) return select_dfuds(")",1,v)-2;
+    return select_dfuds(")",1,v) - select_dfuds(")",1,v-1) - 1;                         
 }
 
 int leaf_rank_dfuds(int v){
+    int g=dfuds[v].val;
+    int h=select_dfuds(")",1,g);
     return rank_dfuds("))",2,v);
 }
 
@@ -242,18 +243,10 @@ int leaf_select_dfuds(int i){
      return select_dfuds("))",2,i);
 }
 
-int inorder_rank_dfuds(int v){
-    return leaf_rank_dfuds(child_dfuds(v,2)-1);
+
+int leftmost_leaf_dfuds(int i){
+    int v=dfuds[i].val;
+    int h=select_dfuds(")",1,v);
+    return leaf_select_dfuds(leaf_rank_dfuds(h-1)+1);
 }
 
-int inorder_select_dfuds(int i){
-    return parent_dfuds(leaf_select_dfuds(i)+1);
-}
-
-int leftmost_leaf_dfuds(int v){
-    return leaf_select_dfuds(leaf_rank_dfuds(v-1)+1);
-}
-
-int rightmost_leaf_dfuds(int v){
-    return find_close_dfuds(enclose_dfuds(v));
-}
--- depth_first_unary_degree_sequence.h
+++ depth_first_unary_degree_sequence.h
@@ -12,14 +12,9 @@ int rank_dfuds(char* pattern,int size,int i);
 int select_dfuds(char* pattern,int size,int i);
 
 int parent_dfuds(int v);
-int child_dfuds(int v, int i);
-int subtreesize_dfuds(int v);
+int last_child_dfuds(int v);
+int first_child_dfuds(int v);
 int degree_dfuds(int v);
-int preorder_rank_dfuds(int v);
-int preorder_select_dfuds(int i);
 int leaf_rank_dfuds(int v);
 int leaf_select_dfuds(int i);
-int inorder_rank_dfuds(int v);
-int inorder_select_dfuds(int i);
 int leftmost_leaf_dfuds(int v);
-int rightmost_leaf_dfuds(int v);
